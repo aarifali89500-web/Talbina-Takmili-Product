3a5aedc772ad0b9ced03b1c006d0b98e34e6a059
aarifali89500-web <aarifali89500@gmail.com>
2025-08-14 15:05:51 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = "talbina-v1";
+const ASSETS = [
+  "./","./index.html","./style.css","./app.js",
+  "./admin.html","./admin.js","./manifest.webmanifest",
+  "./assets/icon-192.png","./assets/icon-512.png","./assets/placeholder.jpg"
+];
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));
+});
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
